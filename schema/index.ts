f74e6fe32e9c4a55716d05fc075c9180d54472bd
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-24 21:14:39 +0500

    modified error msg

--- schema/index.ts
+++ schema/index.ts
@@ -2,8 +2,8 @@ import z from 'zod'
 
 
 export const loginSchema = z.object({
-    email: z.string().email({ message: "invalid email" }),
-    password: z.string().min(6, { message: "invalid password" })
+    email: z.string().email({ message: "Invalid email" }),
+    password: z.string().min(6, { message: "Invalid password" })
 })
 
 export type typeLoginSchema = z.infer<typeof loginSchema>
@@ -11,7 +11,7 @@ export type typeLoginSchema = z.infer<typeof loginSchema>
 
 export const registerSchema = z.object({
     name: z.string().min(2, { message: "Name of 2 characters is required" }),
-    email: z.string().email({ message: "invalid email" }),
+    email: z.string().email({ message: "Invalid email" }),
     password: z.string().min(6, { message: "Password of 6 characters is required" })
 })
 

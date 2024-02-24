7f20f6de1fc15c0afaaad45c3bccf1c928498123
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-24 21:08:57 +0500

    Added register schema

--- schema/index.ts
+++ schema/index.ts
@@ -7,3 +7,12 @@ export const loginSchema = z.object({
 })
 
 export type typeLoginSchema = z.infer<typeof loginSchema>
+
+
+export const registerSchema = z.object({
+    name: z.string().min(2, { message: "Name of 2 characters is required" }),
+    email: z.string().email({ message: "invalid email" }),
+    password: z.string().min(6, { message: "Password of 6 characters is required" })
+})
+
+export type typeRegisterSchema = z.infer<typeof registerSchema>

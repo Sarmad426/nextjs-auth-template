1ccf8d2c0285f9db10ada4e9c4ec92858537c832
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-24 17:33:58 +0500

    Sign in page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,21 @@
+import { CardWrapper } from "@/components/ui/card-wrapper";
+import { Heading } from "@/components/ui/heading";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+  description: "Sign in (🔐 Auth)",
+};
+
+export default function SignInPage() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4 w-full">
+      <Heading title="🔐 Auth" />
+      <CardWrapper
+        title="Welcome back"
+        accountText="Don't have an account? "
+        url="sign-up"
+      />
+    </div>
+  );
+}

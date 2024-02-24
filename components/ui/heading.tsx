92f42ea2fb8e2626198de55123211f3756fc5f5b
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-24 16:48:46 +0500

    Custom heading component

---
+++ components/ui/heading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type HeadingProps = {
+  title: string;
+  description?: string;
+};
+
+export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold my-2">{title}</h2>
+      <p className="text-secondary-foreground my-2">{description}</p>
+    </div>
+  );
+};

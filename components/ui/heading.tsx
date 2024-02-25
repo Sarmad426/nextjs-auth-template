98a312b3b4597fc2643bdb4c73d6bd4095f461c3
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-25 10:11:45 +0500

    Added top margin

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -7,7 +7,7 @@ type HeadingProps = {
 
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
-    <div>
+    <div className="mt-10">
       <h2 className="text-3xl font-semibold my-2">{title}</h2>
       <p className="text-secondary-foreground my-2">{description}</p>
     </div>
